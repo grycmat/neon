e9c6f4f313931f68d1816df05ffd9d81e71110cf
Mateusz Gryc <mateusz@gryc.dev>
2026-08-02 13:56:07 +0200

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.gigapingu.neon"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.1.1"
     }
 
     buildTypes {
