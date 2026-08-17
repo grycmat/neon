31833d15856870d9f9672cd0d9731fe831ee081e
Mateusz Gryc <mateusz@gryc.dev>
2026-08-17 13:54:46 +0200

    version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.gigapingu.neon"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.4.0"
+        versionCode = 10
+        versionName = "1.5.0"
     }
 
     signingConfigs {
